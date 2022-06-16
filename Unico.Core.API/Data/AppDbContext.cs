0a105c98c70fc8e5b96cb65dc12a2178f3fa5641
andersonmorony <a.de.souza.ramos@avanade.com>
2022-06-16 08:47:47 -0300

    feat: config database

---
+++ Unico.Core.API/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Unico.Core.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Market> Markets { get; set; }
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Unico.Core.API/Data/Market.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unico.Core.API.Data
+{
+    public class Market
+    {
+        public int Id { get; set; }
+        public string LONG { get; set; }
+        public string LAT { get; set; }
+        public long SETCENS { get; set; }
+        public long AREAP { get; set; }
+        public int CODDIST { get; set; }
+        public string DISTRITO { get; set; }
+        public int CODSUBPREF { get; set; }
+        public string SUBPREFE { get; set; }
+        public string REGIAO5 { get; set; }
+        public string REGIAO8 { get; set; }
+        public string NOME_FEIRA { get; set; }
+        public string REGISTRO { get; set; }
+        public string LOGRADOURO { get; set; }
+        public int? NUMERO { get; set; }
+        public string BAIRRO { get; set; }
+        public string REFERENCIA { get; set; }
+    }
+}
