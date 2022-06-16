a9494d166425c0e069004a826c9fe54984e6e706
andersonmorony <a.de.souza.ramos@avanade.com>
2022-06-16 09:20:17 -0300

    feat: add seed to test

--- Unico.Core.API/Services/MarketService.cs
+++ Unico.Core.API/Services/MarketService.cs
@@ -1,20 +1,34 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Unico.Core.API.Data;
 using Unico.Core.API.Models;
 
 namespace Unico.Core.API.Services
 {
     public  class MarketService
     {
-        public MarketService()
+        private readonly AppDbContext _dbContext;
+
+        public MarketService(AppDbContext dbContext)
         {
+            _dbContext = dbContext;
         }
 
-        public async Task<MarketRequest> GetMarketsAsync()
+        public async Task<(IEnumerable<Market> markets, bool IsSuccess, string MsgError)> GetMarketsAsync()
         {
-            var response = new MarketRequest();
+            
+            var response = await _dbContext.Markets.ToListAsync();
 
-            return response;
+            if(response.Any())
+            {
+                return (response, true, null);
+            }
+            return (null, false, "No Data");
+
         }
     }
 }
--- Unico.Core.Test/Services/MarketRequestServiceTests.cs
+++ Unico.Core.Test/Services/MarketRequestServiceTests.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using Unico.Core.API.Data;
 using Unico.Core.API.Services;
 using Xunit;
 
@@ -8,14 +9,38 @@ namespace Unico.Core.Test.Services
 {
     public class MarketRequestServiceTests
     {
+        private readonly AppDbContext _dbContext;
+
+        public MarketRequestServiceTests()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>().UseInMemoryDatabase(nameof(ShouldReturnMarket)).Options;
+            _dbContext = new AppDbContext(options);
+            seedData();
+        }
+
         [Fact]
         public async void ShouldReturnMarket()
         {
-            var _marketService = new MarketService();
-           
+            var _marketService = new MarketService(_dbContext);
+
             var result = await _marketService.GetMarketsAsync();
 
-            Assert.NotNull(result);
-        } 
+            Assert.True(result.IsSuccess);
+            Assert.True(result.markets.Any());
+            Assert.Null(result.MsgError);
+
+        }
+        private void seedData()
+        {
+            if (!_dbContext.Markets.Any())
+            {
+                _dbContext.Markets.AddRange(new List<Market>()
+                {
+                    new Market() { Id = 1, LONG = "-46550164", SETCENS = 355030885000091, AREAP = 3550308005040, CODDIST = 87, DISTRITO = "VILA", CODSUBPREF = 26, SUBPREFE = "VILA PRUDENTE", REGIAO5 = "LESTE", REGIAO8 = "LESTE 1", NOME_FEIRA = "VILA FONSECA", REGISTRO = "4041-0", LOGRADOURO = "RUA X", BAIRRO = "SP", LAT = "-23558733", NUMERO = 15, REFERENCIA = "" },
+                    new Market() { Id = 2, LONG = "-4655024", SETCENS = 35503020091, AREAP = 1308005040, CODDIST = 17, DISTRITO = "SAO MIGUEL", CODSUBPREF = 26, SUBPREFE = "VILA PRUDENTE", REGIAO5 = "LESTE", REGIAO8 = "LESTE 1", NOME_FEIRA = "VILA SAO MIGUEL", REGISTRO = "4041-0", LOGRADOURO = "RUA X", BAIRRO = "SP", LAT = "-23558733", NUMERO = 15, REFERENCIA = "" }
+                });
+                _dbContext.SaveChanges();
+            }
+        }
     }
 }
